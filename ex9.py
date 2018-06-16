889f034d64e2d896a0e2f66bc7fcb20be44c9768
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-16 11:13:17 +0630

    ex9.py

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days="Mo Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print("Here are the days :",days)
+print("Here are the months",months)
+print("""
+        There's something going on here.
+        With the three double_quotes.
+        We'llbe able to type as much as we like.
+        Even 4lines if we want ,or 5 or 6.
+        """)
+
