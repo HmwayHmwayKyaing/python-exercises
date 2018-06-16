1cefd120400fa06a6ebfb799276e97d11aac7def
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-16 10:14:05 +0630

    yel

--- ex4.py
+++ ex4.py
@@ -9,7 +9,7 @@ average_passengers_per_car=passengers/cars_driven
 print("There are ",cars,"cars available.")
 print("There are only",drivers,"drivers available")
 print("There will be",cars_not_driven,"empty cars today")
-print("We can transport ", carpool_capaicity,"people today.")
+print("We can transport ", carpool_capacity,"people today.")
 print("We have ",passengeers,"to carpool today")
 print("We neeed to put about", average_passengers_per_car,"in each car")
 
