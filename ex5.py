6c108e99e89e6c74da0983a0fb90b5bbb2d3d204
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-16 10:04:58 +0630

    pel

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name='Zed A.Shaw'
+my_age=35#not a line
+my_height=74#inches
+my_weight=180#lbs
+my_eyes='Bllue'
+my_teeth='Whitw'
+my_hair='Brown'
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height}inhes tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes}eyes and {my_hair}hair.")
+total=my_age+my_height+my_weight
+print(f"If I add {my_age},{my_height},and {my_weight} I get {total}.")
+
