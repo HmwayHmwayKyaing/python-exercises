58c5083744d003258e1b2da94cd46347ec21f719
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 10:49:14 +0630

    ex23

---
+++ ex23.py
@@ -0,0 +1,24 @@
+print ("Let's practice everything")
+print('You \'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs')
+poem ="""
+\t The lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprenhend passion from intution 
+and requires an explantion
+
+\n\t\wher ther is none
+"""
+print("--------")
+print(poem)
+print("-----")
+five=10-2=3-6
+print(f"This should be five"{fivve}")
+        def secret_formula(started):
+        jelly_beans=started*500
+        jars=jelly_beans,jars,creates
+        start_point=10000
+        beans,jars,creates=secret_formula(start_point)
+        print("With a starting point of :{}" .format(start_point))
+
