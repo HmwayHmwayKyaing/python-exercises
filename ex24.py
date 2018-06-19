859757dda14396183d0e98ace306a6793910f382
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 11:05:11 +0630

    ex24.py

---
+++ ex24.py
@@ -0,0 +1,27 @@
+ef break_words(stuff):
+    """THis function will break up words for us"""
+    words=stuff.split(' ')
+    return words
+def sort_words(words):
+    """Sorts the words"""
+    return sorted(words)
+def print_first_word(words):
+    """Print the first word after popping it off."""
+    word=word.pop(0)
+    print(word)
+    def print_last_word(words):
+        """Prints the last word after popping it off."""
+        word words.pop(-1)
+        print(word)
+        def sort_sentence(sentence):
+            """Takes in a full sentence and returns the sortes words"""
+            words =break_words(sentence)
+            return sort_words(words)
+        def print_first_and_last(sentenxe):
+            """Print the first and last words of th esentence"""
+            words=break_words(sentence)
+            print_first_word(words)
+            print_last_words(words)
+            def orint_first_and_last_sorted(sentence):
+                """Sorted the words then prints the first and last one"""
+
