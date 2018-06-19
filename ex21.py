b3dae84e4334330814155a737df82cd602fdb0db
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 10:19:52 +0630

    ex21

---
+++ ex21.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    prin(f"ADDING{a}+{b})
+    return a+b
+    def subtract(a,b):
+    print(f"MULTIPLYING {a}*{b}")
+    return a*b
+    def divide(a,b)
+    print(f"DIVIDING {A} /{B}")
+    print("Let's do some math with just function")
+    age =add(30,5)
+    height =subtract(78,4)
+    weight=multipy(90,2)
+
+    iq=divide(100,2)
+    print(f"Age:{age},Height:{height},Weigth:{weight},IQ:{iq}")
+print("Here is a puzzle")
+what=add(age,subtract(height,multiply(weigth,divide(iq,2)))
+print("That becomes:",what,"Can you do it hand?")
+
