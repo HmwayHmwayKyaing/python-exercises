ab2e06855b29884826e6b9bf5422985bd205ddf2
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 11:41:25 +0630

    ex26

---
+++ ex26.py
@@ -0,0 +1,21 @@
+people =20
+cats =30
+dogs=15
+if people<cats:
+    print("Too many cats! THe world is doomed!")
+    if peodrooled on!")
+  if people >cats:
+        print("Not many cats ! The world is saved!")
+        if people<dogs:
+            print("The world is drolled on!")
+            if people>dogs
+            print("The world is dry!")
+            dogs+=5
+            if people>=dogs:
+            print("People are gather than or equal to dogs")
+            if people<=dogs:
+            print("People are less than or equal to dogs")
+            if people=dogs:
+            printt("People are dogs")
+
+
