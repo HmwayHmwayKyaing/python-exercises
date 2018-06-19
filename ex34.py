3e83aea8b5033547687265d6325a4e6a991bb8c1
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 13:27:13 +0630

    ex34

---
+++ ex34.py
@@ -0,0 +1,24 @@
+ def apple():
+     print("I AM APPLES")
+     class MyStuff(object:
+             def__init__(self):
+             self.tangerine="And now a thousand years between"
+             def apple(self):
+             print("I AM CLASSY APPLES")
+            thing=MyStuff()
+            thing.apple()
+            print(thing.tangerine)
+            class Song(object):
+            def___init__(self,lyrics):
+            self.lyrics=lyrics
+            sef sing_me_a_song(self):
+            for line in self.lyrics:
+            print(line)
+            happy_bday=Song(["Happy birthday to you "
+                ,"I don't want to get sued",
+                "So I'll stop right there"])
+            happy_bday.sing_me_a_si=ong()
+
+
+
+             
