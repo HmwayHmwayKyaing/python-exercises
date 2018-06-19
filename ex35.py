59420b65c40adb944e3904a21d9e2f3511179e7d
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 13:59:39 +0630

    35

---
+++ ex35.py
@@ -0,0 +1,12 @@
+import random
+from urllib.request import urlopen
+import sys'WORD_URL="http://learncodethehardway.org/words.txt"
+WORDS=[]
+PHRASEES={
+"class %%%(%%%):":
+    "Make a class named %%% that is -a%%%",
+    "class %%%(object):\n\tdef ___init___(self,***)":
+    "class %%% has -a ___init___that takes self and *** params",
+    "class %%%(obkect):\n\tdef ***(self,@@@)":
+    }
+
