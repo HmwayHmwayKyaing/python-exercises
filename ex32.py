bcdac60956f2572cd4a6a56d38f55288e73fe6a3
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 13:03:53 +0630

    ex32

---
+++ ex32.py
@@ -0,0 +1,17 @@
+ten_things="Apple Oranges Crows Telephones  Light Sugar"
+print ("Wait there are not 10 things in that list .Let's fix that ")
+stuff =ten _things.split(' ')
+more_stuff=["Day","Night ","Song","Frisbee",
+        "Corn","Banana" "Girl","Boys"]
+while len(stuff)!=10:
+    next_one=more_stuff.pop()
+    print("Adding",next_one)
+    stuff.append(next_one)
+    print(f"There are len(stuff)} items now")
+    print("There wwe go " ,stuff)
+    print("Let's do something with stuff")
+    print(stuff[1])
+    print(stuff[-1])print(stuff.pop())
+    print(''.join(stuff))
+    print('#'.join(stuff[3:5]))
+
