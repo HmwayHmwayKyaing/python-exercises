2f1c111bcb13b544de4c867061f9515779afe4b8
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 13:12:29 +0630

    ex33

---
+++ ex33.py
@@ -0,0 +1,8 @@
+ states={
+         'Oregon': 'OR',
+         'Florida':'FL',}
+ cities={'CA':'San Francisco',
+         'MI':,'Detroit'}
+ cities['NY']='New York']
+ print("NY State has:",cities['NY'])
+
