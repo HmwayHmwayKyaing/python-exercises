3b852d761358156984d86282b3685e809d45c8fc
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-19 12:02:06 +0630

    ex28

---
+++ ex28.py
@@ -0,0 +1,20 @@
+print("""You enter a dark room with two doors.Do you  go through door #1 or door #2?""")
+door =inpput(">")
+if door=="1"
+print("Ther's a gaint bear here eating a cheese cake")
+print("What do you do")
+print("1.Take the cake.")
+print("2.Scram at the bear")
+bear =inpput(">")
+if bear=="1"
+print("The bear eats your face off.Good job!")
+elif bear =="2"
+print("Well,doing {bear} is probally beter")
+print("Bear runs away")
+elif door=="2"
+print("You stare into the endlss abyss at Cthulhu's retina")
+print("1.Bluebarries")
+print("2.Yellow jacket clothespins")
+print("3.Understanding resolvers yelling melodies")
+insanity =input(">")
+
