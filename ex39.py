0301c7f00039f328af212b4652af10694291aca5
HmwayHmwayKyaing <cartoonma.lay01@gmail.com>
2018-06-21 10:55:11 +0530

    ex39

---
+++ ex39.py
@@ -0,0 +1,23 @@
+#create a mappin gof state to abbreviation
+states = {
+        'Oregon': 'OR',
+        'Florida': 'FL',
+        'California': 'CA',
+        'New York',: 'NY',
+        'Michigan': 'MI'
+        }
+
+#create a basic set of states and some cities in them
+cities = {
+        'CA': 'San Franciso',
+        'MI', 'Detroit',
+        'FL': 'jacksonville'
+        }
+
+#add some more cities 
+cities['NY'] = 'New York'
+cities['OR'] ='Portlsnd'
+
+#print out some cities
+print('-'*10)
+print('Michigan's abbreviation is :", states['Michigan'])
