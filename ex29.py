d917674b3a154d655512d25e625cde66f94ae6c2
Hmway Hmway Kyaing <cartoonma.lay01@gmail.com>
2018-06-25 20:51:32 +0530

    Update ex29.py

--- ex29.py
+++ ex29.py
@@ -1,15 +1,26 @@
-the_count=[1,2,3,4,5]
-fruita=['apples','oranges','pears','apricots']
-change=[1,'pennies',2,'dimes',3,'quarters']
-for number in the_count:
-    print(f"This is count {number}")
-    for fruit in fruits:
-        print(f"A fruit of type:{fruits}")
-        for i in change:
-            print(f"I got {i}")
-            elements=[]
-            for i in range(0,6):
-                print(f"Adding {i} to the list")
-                for i in the elements:
-                    print(f"Element was :{i}")
+people=20
+cats=30
+dogs=15
 
+
+if people<cats:
+    print("Too many cats!The world is doomed!")
+
+if people>cats:
+    print("Not many cats!The world is saved!")
+if people<dogs:
+    print("The world is drooled on!")
+
+
+dogs+=5
+
+if people>=dogs:
+    print("People are greater than or equal to dogs.")
+
+
+if people<=dogs:
+    print("People are less than or equal to dogs.")
+
+
+    if people==dogs:
+        print("People are dogs.")
