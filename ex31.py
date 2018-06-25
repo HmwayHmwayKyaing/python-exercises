33e21ea30e4f2e1b59da30d36125b2a7db0962e5
Hmway Hmway Kyaing <cartoonma.lay01@gmail.com>
2018-06-25 20:53:39 +0530

    Update ex31.py

--- ex31.py
+++ ex31.py
@@ -1,60 +1,25 @@
-fromm sys import exit
-def gold_room():
-    print("This is full of gold.How much dod you take?")
-    choice=input(">")
-    if "0" in choice or "1" in choice:
-        how_much =int(choice)
+print ("""You enter a dark room with two doors.
+Do you go through door #1 or door#2?""")
+
+door=input(">")
+
+if door=="1":
+    print("There's a giant bear here eating a cheese cake.")
+    print("What do you do?")
+    print("1.Take the cake.")
+    print("2.Scream at the bear.")
+
+    bear=input(">")
+
+    if bear=="1":
+        print("The bear eats your face off.Good job!")
+    elif bear=="2":
+        print("The bear eats your legs off. Good job!")
     else:
-        dead("Man.learn to type a number ")
-        if how_much <50:
-            print("Nice, you're not greedy ,you win!")
-            exit(0)
-        else:
-            dad("You greedy bastard")
-            def bear_room():
-                print("There's is a bear here")
-                print("The bear has a bunch of honey")
-                print("The fat bear is in front of another door")
-                bear_moved=False"
-                while True:
-                    choice=input(">")
-                    if choice=="take honey":
-                    dead("The bear looks at you then slaps your face off")
-                    elif choice=="taunt bear " and not bear_moved:
-                    print("The bear has moved from the door")
-                    print("You can go through it now")
-                    bear_moveed=True
-                    elif choice=="taunt berar" and bear_moved:
-                    dead("The bear gets pissed off and chews your leg off")
-                    elif choice=="open door" and bear_moved:
-                    gold_room()
-                    else:
-                    print("I got idea what that means")
-                    def cthulhu_room():
-                    print("HerHe,e you see that great wvil Cthulhhu")
-                    print("He, it ,whatever stares at you and you go  insane")
-                    print("Do you flee for your life or eat your head?")
-                    choice=input(">")
-                    if "flee" in choice:
-                    start()
-                    elif "head" in choice:
-                    dead("Well that was tasty")
-                    else:
-                    cthulhu_room()
-                    def dead(why):
-                    print(why,"Good job")
-                    exit(0)
-                    def start():
-                    print("You are in a dark room")
-                    print("There is a right and left ")
-                    choice=input(">")
-if choice=="left":
-    bear_room()
-    elif choice=="right"
-    cthulhu_room()
-    else:
-    dead("You stumble around th eroom until you starve")
-    start()
+        print(f"Well, doing {bear} is probably better.")
+        print ("Bear runs away.")
 
-
-
+    elif door=="2":
+        print("You stare into the endless abyss at Cthulhu's ret
+        print("1.Blueberries.")
+        print("2. Yellow jacket clothespins.")
