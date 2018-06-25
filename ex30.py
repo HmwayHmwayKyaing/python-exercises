9d11f06c95cba10aed8b959a882edce02b7eaf0f
Hmway Hmway Kyaing <cartoonma.lay01@gmail.com>
2018-06-25 20:52:43 +0530

    Update ex30.py

--- ex30.py
+++ ex30.py
@@ -1,12 +1,23 @@
-i=0
-numbers=[]
-while i<6:
-    print(f" At the top i is {i}")
-    numbers.append(i)
-    i=i+1
-    print("Numbers now :",numbers)
-    print(f"At he bottom i is {i}")
-    print("The numbers:")
-    for num in numbers:
-        print(num)
+people=30
+cars=40
+trucks=15
 
+
+if cars>people:
+    print ("We should take the cars.")
+elif cars<people:
+    print("We should not take the cars.")
+else:
+    print("We can't decide.")
+
+if trucks>cars:
+    print("That's too many trucks.")
+elif trucks<cars:
+    print("Maybe we could take the trucks.")
+else:
+    print ("We still can't decide.")
+
+    if people>trucks:
+        print("Alright,let's just take the trucks.")
+    else:
+        print ("Fine, let's stay home then.")
