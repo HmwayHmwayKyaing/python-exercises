0c961657aad66ecadbc24b8a91b8f344e59a6206
Hmway Hmway Kyaing <cartoonma.lay01@gmail.com>
2018-06-26 14:13:59 +0530

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,18 +1,18 @@
-tabby_rat="\t I'm tabbed in"
-persian_rat="I'm split\non a line"
-backslash_rat="I'm \\a\\rat"
+tabby_cat="\t I'm tabbed in"
+persian_cat="I'm split\non a line"
+backslash_cat="I'm \\a\\cat"
 
-fat_rat="""
+fat_cat="""
 I'll do a list:
-\t*Rat food
+\t*Cat food
 \t*Fishes
-\t*Ratnip\n\t*Grass
+\t*Catnip\n\t*Grass
 """
 
-print(tabby_rat)
-print(persian_rat)
-print(backslash_rat)
-print(fat_rat)
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
 
 
 
